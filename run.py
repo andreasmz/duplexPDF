9219686aa0ba6aa308e93f373587799486ef6713
andreasmz <174248270+andreasmz@users.noreply.github.com>
2025-01-11 17:52:17 +0100

    added update to run script

--- run.py
+++ run.py
@@ -2,11 +2,15 @@
 
 import subprocess
 import sys
+from packaging.version import Version
 
 print("Starting DuplexPDF by Andreas B.")
 
 try:
     import duplexPDF
+
+    if Version(duplexPDF.__version__) <= Version("1.0.1"):
+        print(subprocess.check_call([sys.executable, "-m", "pip", "install", "duplexPDF", "--upgrade"]))
 except ModuleNotFoundError:
     print(subprocess.check_call([sys.executable, "-m", "pip", "install", "duplexPDF"]))
 
